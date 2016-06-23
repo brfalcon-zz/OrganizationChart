a0126d561b6f2650e76d2bc47e2a56ac6fd37574
TIN_1 <TIN_1@DESKTOP-Q3RO6OT>
2016-06-23 01:04:25 -0300

    Add project files.

---
+++ OrganizationChart/OrganizationChart.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationChart
+{
+    public class OrganizationChart
+    {
+        public Employee Manager { get; set; }
+        private Queue<Employee> Employees;
+
+        public OrganizationChart()
+        {
+            Manager = new Employee("Organization Chart");
+            Employees = new Queue<Employee>();
+        }
+
+        public string PrintTreeView()
+        {
+            if (Manager == null)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder tree = new StringBuilder();
+
+            Employees.Enqueue(Manager);
+
+            int spacing = 0;
+            Employee currentManager = null;
+
+            while (Employees.Count > 0)
+            {
+                Employee employee = Employees.Dequeue();
+
+                if (employee.Manager != null && !employee.Manager.IsPeer(currentManager))
+                {
+                    spacing = 0;
+                    currentManager = employee.Manager;
+                    tree.AppendLine();
+                }
+                                                          //Remover depois. Efeito cosmetico        
+                spacing += CalculateSpacing(employee) / 2 - employee.Name.Length / 2;
+
+                tree.AppendFormat("{0}{1}", MakeIdentation(spacing), employee.Name);
+                
+                foreach (var subordinate in employee.Subordinates)
+                {
+                    subordinate.Manager = employee;
+                    Employees.Enqueue(subordinate);
+                }
+                
+            }
+
+            return tree.ToString() ;
+        }
+
+        private string MakeIdentation(int identationLength)
+        {
+            string identation = " ";
+
+            for (int i = 0; i < identationLength; i++)
+            {
+                identation += " ";
+            }
+
+            return identation;
+        }
+
+        public int CalculateSpacing(Employee employee)
+        {
+            int subordinateCount = employee.Subordinates.Count;
+            int width = employee.Name.Length;
+
+            for (int i = 0; i < subordinateCount; i++)
+            {
+                var subordinate = employee.Subordinates[i];
+                width += CalculateSpacing(subordinate);
+            }
+
+            return width < 0 ? 0 : width;
+        }
+
+        private string PrintSubTreeView(Employee employee, string identation)
+        {
+            int subordinateCount = employee.Subordinates.Count;
+
+            StringBuilder subTree = new StringBuilder();
+
+            subTree.AppendLine(identation + employee.Name);
+
+            for (int i = 0; i < subordinateCount; i++)
+            {
+                var subordinate = employee.Subordinates[i];
+                subTree.Append(PrintSubTreeView(subordinate, identation + "   "));
+            }
+
+            return subTree.ToString();
+        }
+    }
+
+    public class Employee
+    {
+        public Employee(string name)
+        {
+            this.Name = name;
+            this.Subordinates = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+        public Employee Manager { get; set; }
+        public List<Employee> Subordinates { get; set; }
+        public bool IsManager { get; private set; }
+        public bool HasSubordinates { get { return Subordinates != null && Subordinates.Count > 0; } }
+
+        public void AddSubordinate(Employee employee)
+        {
+            if(IsManager == false)
+            {
+                IsManager = true;
+            }
+
+            Subordinates.Add(employee);
+        }
+
+        public bool IsPeer(Employee employee)
+        {
+            if(employee == null)
+            {
+                return false;
+            }
+
+            return this.Manager == employee.Manager;
+        }
+
+    }
+
+}
---
+++ OrganizationChart/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrganizationChart chart = new OrganizationChart();
+
+            Employee felix = new Employee("aaaaa");
+            Employee x = new Employee("bbbbb");
+            Employee x1 = new Employee("ccccc");
+            Employee x2 = new Employee("ddddd");
+            Employee rubiana = new Employee("eeeee");
+            Employee vinicius = new Employee("fffff");
+            Employee vinicius1 = new Employee("ggggg");
+            Employee iury = new Employee("hhhhh");
+
+            chart.Manager = felix;
+
+            felix.AddSubordinate(rubiana);
+            felix.AddSubordinate(x);
+            x.AddSubordinate(x1);
+            x.AddSubordinate(x2);
+            rubiana.AddSubordinate(vinicius);
+            rubiana.AddSubordinate(iury);
+            vinicius.AddSubordinate(vinicius1);
+            vinicius.AddSubordinate(vinicius1);
+
+            Console.WriteLine(chart.PrintTreeView());
+            Console.Read();
+        }
+    }
+}
